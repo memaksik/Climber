ee952d248cff3f48f591bb600ecf552179572a23
Maxim <Registr.Masik@yandex.ru>
2020-01-11 01:53:49 +0300

    -create person construct;

---
+++ Climb/Assets/Scripts/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    private float power;
+    private String name;
+
+    public Person(float power, String name)
+    {
+        this.name = name;
+        this.power = power;
+    }
+
+    public void SetName(String name)
+    {
+        this.name = name;
+    }
+
+    public void SetPower(float power)
+    {
+        this.power = power;
+    }
+
+    public float GetPower()
+    {
+        return power;
+    }
+
+    public String GetName()
+    {
+        return name;
+    }
+    
+ 
+}

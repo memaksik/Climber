5b1809afdeeb374ab030529279d92a2e33ead53b
Maxim <Registr.Masik@yandex.ru>
2020-01-11 13:46:12 +0300

    - added background move;

---
+++ Climb/Assets/Scripts/Enviroment/MoveBackGround.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackGround : MonoBehaviour
+{
+    public float deltaY;
+    public GameObject Background;
+        public void Move()
+        {
+            Background.transform.position = new Vector2(transform.position.x , transform.position.y + deltaY);            
+        }
+
+}
---
+++ Climb/Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class StartGame : MonoBehaviour
+{
+    private GameObject Background;
+    void Start()
+    {
+        MoveBackGround backGround = new MoveBackGround();
+        backGround.Move();
+    }
+
+}
